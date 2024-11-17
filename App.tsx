28e29801c49731083763435e7944dc35f0d20069
root <root@DESKTOP-JJF3S7I>
2024-11-17 23:37:59 +0500

    added further functionality

--- App.tsx
+++ App.tsx
@@ -5,36 +5,22 @@ import Countdown from "./src/components/Countdown";
 
 export default function App() {
   const [taskList, setTaskList] = useState([]);
+  const [timeList, setTimeList] = useState([]);
   const [currentlyFocusing, setCurrentlyFocusing] = useState(false);
-  // console.log(currentSeconds);
+  // const [stop, setStop] = useState(false);
   return(
     <SafeAreaView style={styles.container}>
-      {/* { !taskName ? (<Focus addTask={setTaskName}/>) : ( currentSeconds !== 0 ? 
-        <SafeAreaView style={styles.counter}>
-          <Text style={{fontSize: 32, fontWeight: 'bold', margin: 12}}>I am going to focus on {taskName}</Text>
-          <Countdown startSeconds={10} setCurrentSeconds={setCurrentSeconds} />
-        </SafeAreaView>
-
-        :
-
-        <Text>
-          All done!
-        </Text>
-      ) } */}
       {!currentlyFocusing ? 
         <SafeAreaView style={styles.container}>
           <Focus addTask={setTaskList} existingTasks={taskList} focusStart={setCurrentlyFocusing} /> 
-          {taskList.map((v) => <Text>Focused on {v}</Text>)}
+          {taskList.map((v, i) => <Text key={i}>Focused on {v} for {timeList[i]}s</Text>)}
         </SafeAreaView>
         : 
       <SafeAreaView style={styles.counter}>
         <Text>Focusing on {taskList[taskList.length - 1]}</Text>
-        <Countdown setFocusStatus={setCurrentlyFocusing} />
+        <Countdown setTimeList={setTimeList} timeList={timeList} setFocusStatus={setCurrentlyFocusing} />
       </SafeAreaView>}
     </SafeAreaView>
-    // <View style={styles.counter}>
-    //   <Countdown startSeconds={100} />
-    // </View>
   );
 }
 
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,20 +4,28 @@ import type { PropsWithChildren } from "react";
 
 type CountdownProps = {
     setFocusStatus: (currentlyFocusing: boolean) => void;
+    setTimeList: (timeList: number[]) => void;
+    timeList: number[];
 }
 
-export default function Countdown({ setFocusStatus }: CountdownProps) {
+export default function Countdown({ setTimeList, timeList, setFocusStatus }: CountdownProps) {
     const intervalRef = useRef(null);
     const [seconds, setSeconds] = useState(10);
     const [startTimer, setStartTimer] = useState(false);
+    const [pause, setPause] = useState(true);
+    const [stop, setStop] = useState(false);
     const timerVals = [10, 15, 20];
+    const totalSeconds = useRef(10);
 
     useEffect(() => {
-        if (startTimer) {
+        if (startTimer || !pause) {
             intervalRef.current = seconds > 0 && setInterval(() => {
                 setSeconds(seconds-1);
             }, 1000);
-            seconds == 0 && setFocusStatus(false);
+            if (seconds == 0) {
+                setTimeList([...timeList, totalSeconds.current]);
+                setFocusStatus(false);
+            }
             return () => {
                 clearInterval(intervalRef.current);
             }
@@ -26,34 +34,71 @@ export default function Countdown({ setFocusStatus }: CountdownProps) {
 
     return(
         <SafeAreaView style={styles.container}>
+            <View style={styles.timer}>                
+                <Text style={styles.time}>{seconds}</Text>
+            </View>
+            <SafeAreaView style={styles.buttonsBox}>
             {
-                !startTimer && timerVals.map((v, i) => <TouchableOpacity onPress={() => {
+                timerVals.map((v, i) => <TouchableOpacity style={styles.button} onPress={() => {
                     setStartTimer(true);
+                    setPause(false);
+                    totalSeconds.current = v;
                     setSeconds(v);
+                    // setTimeList([...timeList, v]);
                 }} key={i}>
                     <Text>{v}</Text>
                 </TouchableOpacity>)
             }
-            {startTimer && <View style={styles.container}>                
-                <Text style={styles.time}>{seconds}</Text>
-            </View>}
+            </SafeAreaView>
+            <TouchableOpacity style={styles.button} onPress={() => {
+                setPause(!pause);
+                setStartTimer(pause);
+            }}>
+                <Text>{pause && !startTimer ? "Start" : "Pause"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => {
+                setTimeList([...timeList, (totalSeconds.current - seconds)]);
+                setFocusStatus(false);
+            }}>
+                <Text>Stop</Text>
+            </TouchableOpacity>
+            
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        // flex: 1,
+        flexDirection: 'column',
+        flex: 1,
         borderWidth: 4,
         // backgroundColor: 'aqua',
         borderColor: 'black',
-        height: 100,
-        width: 200,
+        // height: 100,
+        // width: 200,
+        alignItems: 'center',
+        justifyContent: 'center',
         marginBottom: 200
+    },
+    timer: {
+
     },
     time: {
         fontWeight: 'bold',
         fontSize: 64,
         textAlign: 'center'
+    },
+    button: {
+        borderRadius: 999,
+        borderWidth: 2,
+        height: 40,
+        width: 40,
+        // borderColor: 'pink',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10
+    },
+    buttonsBox: {
+        flexDirection: 'row',
     }
 });
